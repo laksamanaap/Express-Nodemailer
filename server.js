5910fb7bf136d21bbca3ffe9c39bc743c90eefd1
laksamanaap <laksamana.arya1412@gmail.com>
2024-03-12 14:57:51 +0700

    feat : init project

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require("body-parser");
+const busboy = require("express-busboy");
+const nodemailer = require("nodemailer");
+
+const port = process.env.PORT || 5000;
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(300);
+app.listen(port);
